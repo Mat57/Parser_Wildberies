beb32ff94bb49374870d8296f0893dd5f9bfa418
Matthew <popkov-ri@mail.ru>
2021-11-05 17:19:17 +0300

    Добавлена многопоточность

--- search_data.py
+++ search_data.py
@@ -2,19 +2,30 @@ from bs4 import BeautifulSoup as bs
 import requests
 import csv
 from search_categories import ua
+import asyncio
+import aiohttp
 
 headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "user-agent": ua.random
 }
 
+list_cards=[]
 
-def get_count_page(url):
-    response = requests.get(url=url, headers=headers)
-    soup = bs(response.text, 'html.parser')
-    main_block = soup.find_all('a', class_='pagination-item pagination__item')
-    number_of_pages = int(main_block[-1].text)
-    return number_of_pages
+async def get_count_page(url):
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url=url, headers=headers)
+        resp = await response.text()
+        soup = bs(resp, 'html.parser')
+        main_block = soup.find_all('a', class_='pagination-item pagination__item')
+        number_of_pages = int(main_block[-1].text)
+
+        tasks = []
+
+        for page in range(1, number_of_pages+1):
+            task = asyncio.create_task(get_data_from_page(session, str(page)))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
 
 
 def save_to_csv(cards):
@@ -26,13 +37,12 @@ def save_to_csv(cards):
             writer.writerow((card['first_coast'], card['second_coast'], card['company'], card['type']))
 
 
-def get_data_from_page(number_of_page, url):
-    list_cards = []
-    number_of_page = 2
-    for i in range(1, number_of_page + 1):
-        new_url = url + '?page=' + str(i)
-        response = requests.get(url=new_url, headers=headers)
-        soup = bs(response.text, 'html.parser')
+async def get_data_from_page(session, page):
+    global list_cards
+    new_url = url + '?page=' + str(page)
+    async with session.get(url=new_url, headers=headers) as response:
+        resp = await response.text()
+        soup = bs(resp, 'html.parser')
         cards = soup.find_all('div', class_='product-card j-card-item')
         for card in cards:
             try:
@@ -60,12 +70,15 @@ def get_data_from_page(number_of_page, url):
                 card_dict['company'] = company
                 card_dict['type'] = type
                 list_cards.append(card_dict)
+
             except:
                 pass
-    save_to_csv(list_cards)
+        print(f"Обработал страницу {page}")
+
 
 
 if __name__ == '__main__':
     url = str(input('Добавьте ссылку на категорию:')).replace('\n', '')
-    number_of_pages = get_count_page(url)
-    get_data_from_page(number_of_pages, url)
+    asyncio.get_event_loop().run_until_complete(get_count_page(url))
+    save_to_csv(list_cards)
+
